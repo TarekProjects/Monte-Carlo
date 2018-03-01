b779efc8b71ad9b76c3989ad9a4d03aa50087331
Tek <35963855+TekProjects@users.noreply.github.com>
2018-03-01 14:46:33 +0100

    Update longstaff.hpp

--- longstaff.hpp
+++ longstaff.hpp
@@ -19,12 +19,12 @@ class Longstaff {
 		~Longstaff(void);
 		
 		vector<double> AmericanLongstaff(	double S0,
-											double K, 
-											double r,
-											double q,
-											double v, 
-											double T,
-											int NT, 
-											int NS,
-											string PutCall);
+							double K, 
+							double r,
+							double q,
+							double v, 
+							double T,
+							int NT, 
+							int NS,
+							string PutCall);
 };
